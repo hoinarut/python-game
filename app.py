4afddb814f6dbd59f6595250d1eed01ccbd04307
Tudor Hoinaru <tudor.hoinaru@gmail.com>
2019-05-27 23:15:03 +0300

    Added player movement

--- app.py
+++ app.py
@@ -22,12 +22,28 @@ class Game:
 
     def run_game_loop(self):
         is_game_over = False
+        direction = 0
+
+        player_character = PlayerCharacter('player.png', 375, 700, 50, 50)
 
         while not is_game_over:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     is_game_over = True
-                print(event)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
+                # print(event)
+
+            self.game_screen.fill(WHITE_COLOR)
+
+            player_character.move(direction)
+            player_character.draw(self.game_screen)
 
             pygame.display.update()
             clock.tick(self.TICK_RATE)
@@ -39,9 +55,24 @@ class GameObject:
         self.image = pygame.transform.scale(object_image, (width, height))
         self.x_pos = x
         self.y_pos = y
+        self.width = width
+        self.height = height
 
     def draw(self, background):
         background.blit(self.image, (self.x_pos, self.y_pos))
+
+
+class PlayerCharacter(GameObject):
+    SPEED = 10
+
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    def move(self, direction):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
 
 
 pygame.init()
